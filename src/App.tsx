74f91dfa02290c02cd3867b10e0320abeef6d674
Vhmarega <57923276+TorugoMarega@users.noreply.github.com>
2025-08-16 15:23:31 -0300

    FEAT: Consumindo api de filmes e retornando em cards na tela.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MovieCard } from './components/MovieCard/MovieCard';
+import type { Movie } from './types/Movie';
+import styles from './App.module.css'; // Vamos criar este arquivo
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+      const fetchMovies = async () => {
+    try{
+      const apiKey = `api_key=${import.meta.env.VITE_API_KEY}`
+      const apiUrl = import.meta.env.VITE_API_URL
+      const endpont = `/movie/popular`
+      const apiLanguage = `language=pt-BR`
+      const url = `${apiUrl}${endpont}?${apiKey}&${apiLanguage}` 
+
+      console.log("Chamando API de filmes na URL: ", url)
+
+      const response = await fetch(url);
+      if(!response.ok){
+        throw new Error('Falha ao buscar os filmes. ');
+      }
+      const data = await response.json();
+      setMovies(data.results);
+    }catch(err: any){
+      setError(err.message); // Em caso de erro, atualiza o estado de erro
+    }
+    finally {
+        setIsLoading(false); // Termina o carregamento, com sucesso ou erro
+      }
+  };
+
+  fetchMovies();
+  }, []) // O array vazio [] garante que este efeito rode apenas uma vez
+
+  if (isLoading) {
+    return <div className={styles.loadingState}>Carregando filmes...</div>;
+  }
+
+  if (error) {
+    return <div className={styles.errorState}>Erro: {error}</div>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    <div>
+      <h1 className={styles.title}>CineInfo</h1>
+      <ul className={styles.moviesGrid}>
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.id} // A prop 'key' é essencial para o React!
+            id={movie.id}
+            title={movie.title}
+            poster_path={movie.poster_path}
+            vote_average={movie.vote_average}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
